96939e9673347d2ce7707ce6ebceedf1ce510a3a
vxxxxc <vxxxxc@Serpents-M3.local>
2025-04-28 02:58:17 +0800

    add alias paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {},
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: `${path.resolve(__dirname, "./src/components")}`,
+      pages: `${path.resolve(__dirname, "./src/pages")}`,
+      assets: `${path.resolve(__dirname, "./src/assets")}`,
+    },
   },
-})
+  define: {
+    "process.env": {},
+  },
+});
